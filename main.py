517e6fd2e21b208e6e8ab829638f0b6b9847f961
Ramazan  TOY <ramcotoy06@gmail.com>
2023-05-14 21:19:23 +0300

    some fixes changes

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import time
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
+    level=logging.WARNING
 )
 
 accountID =12444421 
@@ -25,9 +25,9 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         userName   = update.effective_user.first_name
         userID   = update.effective_user.id
         command, *args = update.message.text.split()
-        admin_warning = "Bu komutu sadece yöneticiler kullanabilir."
+        admin_warning = "Sadece yöneticiler tarafından kullanılabilirim.)."
 
-        args_warning = "<b>Kullanıcıları</b> çağırırken onlara bir mesajın olmasına ne dersin? 🤔\n<b>Örnek komut:</b> /utag Merhabalar\n\nveya istediğiniz mesaja /utag komutunu yanıtlayın"
+        args_warning = "<b>Beni</b> kullanmak için bir mesaj yazmalısın.)"
 
         effective_user = await context.bot.getChatMember(chatID,userID)
         if str(effective_user.status) != "creator" and str(effective_user.status) != "administrator":
@@ -36,36 +36,42 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
             if len(context.args) != 0:
                 userMention = f"<a href='tg://user?id={userID}'>{userName}</a>"
                 starting_message = f"""
-                    Etiket İşlemi {userMention} Tarafından Başlatıldı.\nİyi Sohbetler.
+                    Etiketleme  işlemi {userMention}  tarafından başlatılıyor.....
                 """
 
-                await context.bot.send_message(chatID,starting_message,parse_mode="HTML")
+                await context.bot.send_message(chatID,starting_message,parse_mode="HTML",reply_to_message_id=update.message.id)
 
                 members = await getChatMembers(chatID)
-                botCounter = 0
-                for x in members:
+                membersTemp =members.copy()
+                for x in membersTemp:
                     chatmember = await context.bot.get_chat_member(chatID,x[0])
                     if chatmember.user.is_bot:
                         members.remove(x)
-                        botCounter += 1
-                print(members)
+
                 text=' '.join(args)
                 text= f"<b>{text}</b>\n"
                 header = f"\n{text}"
-               
-                arr = np.array(members)
-                subarrays = np.array_split(arr,len(members)/5)
-                for _members in subarrays:
 
-                    message = f"{header}"
-                    for index,_member in enumerate(_members):
-                       message += f"<a href='tg://user?id={_member[0]}'>{_member[1]}</a>, "
-                    await context.bot.send_message(chat_id=update.effective_chat.id, text=message,parse_mode="HTML")
-                    time.sleep(3.5)
-                print("bitti")
-                finalMessage = f"✅ <b>İşlem Bitti !</b>\n\n👥 Etiketlenen Kullanıcı sayısı : {len(members)}\n🗣 Etiket işlemini başlatan : {userMention}"
+                if len(members) > 10:
+                    arr = np.array(members)
+                    subarrays = np.array_split(arr, len(arr) / 5)
+                    for _members in subarrays:
+                        message = f"{header}"
+                        for index, _member in enumerate(_members):
+                            message += f"<a href='tg://user?id={_member[0]}'>{_member[1]}</a>, "
+                        await context.bot.send_message(chat_id=update.effective_chat.id, text=message, parse_mode="HTML")
+                        time.sleep(3.5)
+                else:
+                    messageText=""
+                    for _member in members:
+                        messageText+=f"<a href='tg://user?id={_member[0]}'>{_member[1]}</a>, "
 
-                await context.bot.send_message(chatID,text=finalMessage,parse_mode="HTML")
+                await context.bot.send_message(chat_id=update.effective_chat.id, text=messageText,
+                                             parse_mode="HTML")
+                finalMessage = f"✅ <b>Etiketleme işlemi tamamlandı.</b>\n\n👥 Etiketlenen Kullanıcı sayısı : {len(members)}\n🗣 Etiket işlemini başlatan : {userMention}"
+                await context.bot.send_message(chatID, text=finalMessage, parse_mode="HTML",reply_to_message_id=update.message.id)
+
+
             else:
                 await context.bot.send_message(chatID,text=args_warning,parse_mode="HTML",reply_to_message_id=update.message.id)
 async def getChatMembers(chatID):
